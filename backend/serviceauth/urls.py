821dd15833714f9328ed9573b465485e4a75b649
mkeyhanim <MkeyhaniDeve@gmail.com>
2024-11-23 01:36:04 +0330

    refactor: create login and forget-password path

--- backend/serviceauth/urls.py
+++ backend/serviceauth/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
-from .views import RegisterUserCreateAPIView
+from .views import Register_User,Login_User,Forget_Password
 
 urlpatterns = [
-    path("register/", RegisterUserCreateAPIView.as_view()),
+    path("register/", Register_User.as_view()),
+    path("login/", Login_User.as_view()),
+    path("forget-password/", Forget_Password.as_view()),
 ]
