41a71d4599c5ebcecaa7b9dde1e834e23e044071
mkeyhanim <MkeyhaniDeve@gmail.com>
2024-11-23 01:38:37 +0330

    fix: get request and it will have JWT , it hasn't write now

--- backend/serviceauth/views.py
+++ backend/serviceauth/views.py
@@ -6,7 +6,16 @@ from rest_framework import status
 from django.contrib.auth import authenticate
 
 
-class RegisterUserCreateAPIView(APIView):
+class Register_User(APIView):
+    def post(self, request):
+        serializer = Register_userSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
+        else:
+            return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+class Login_User(APIView):
     def get(self, request, *args, **kwargs):
         credential = request.data.get('credential')
         password = request.data.get('password')
@@ -14,47 +23,22 @@ class RegisterUserCreateAPIView(APIView):
         if not credential or not password:
             return Response({'error': 'Both credential and password are required.'}, status=status.HTTP_400_BAD_REQUEST)
 
-        try:
-            user = Register_user.objects.filter(email=credential).first()
-                   #  or \
-                   # Register_user.objects.filter(username=credential).first()
-            if not user:
-                return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-        except Register_user.DoesNotExist:
-             return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
+        user = Register_user.objects.filter(username=credential, password=password).exists()
 
-        print(user, " user >>>>>>>>>>>>.")
-
-        authenticated_user = authenticate(username=user, password=password)
-
-        if authenticated_user:
-            return Response({'message': 'Login successful', 'user': authenticated_user.username},
-                            status=status.HTTP_200_OK)
-        else:
+        if user:
+            return Response({'success': True, 'token': 'token'}, status=status.HTTP_200_OK)
+        if not user:
             return Response({'error': 'Invalid credentials'}, status=status.HTTP_401_UNAUTHORIZED)
-        # result = Register_user.objects.all()
-        # serializers = Register_userSerializer(result, many=True)
-        # return Response({'status': 'success', "users": serializers.data}, status=200)
 
+class Forget_Password(APIView):
+    def put(self, request):
+        try:
+            snippet = Register_user.objects.get(username=request.data.get('username'))
+        except Register_user.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
 
-def post(self, request):
-    serializer = Register_userSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response({"status": "success", "data": serializer.data}, status=status.HTTP_200_OK)
-    else:
-        return Response({"status": "error", "data": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
-
-
-def put(self, request):
-    try:
-        print(request.data.get('id'))
-        snippet = Register_user.objects.get(pk=request.data.get('id'))
-    except Register_user.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    serializer = Register_userSerializer(snippet, data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        serializer = Register_userSerializer(snippet, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
