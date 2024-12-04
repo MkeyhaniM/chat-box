56851d6ab0ee16a3df592c4a9273ccd8650c0576
mkeyhanim <MkeyhaniDeve@gmail.com>
2024-12-04 10:07:16 +0330

    style: change style of postions in banner homepage

--- frontend/chat-box/app/page.tsx
+++ frontend/chat-box/app/page.tsx
@@ -6,33 +6,36 @@ const GeistMono: NextFont = Geist_Mono({
   weight: "400",
 });
 
-import {NavbarHome} from "@/src/components";
+import {Button, NavbarHome} from "@/src/components";
 import "./globals.css";
 
 export default function Home() {
   return (
     <section className="">
-      <div className="bg-black h-screen relative flex flex-col gap-[160px]">
+      <div className="h-screen relative flex flex-col sm:gap-[50px] md:gap-[210px] lg:gap-[160px] overflow-y-hidden">
         <div>
           <NavbarHome />
         </div>
-        <div className="text-white flex justify-center items-center flex-col z-10 gap-5 px-2 lg:px-0">
-          <h1 className="font-bold text-xl lg:text-5xl bg-gradient-to-r from-[#C939CB] via-[#bfacfcfa] to-[#6a84fdf4] bg-clip-text text-transparent">
+        <div className="flex justify-center items-center flex-col z-10 gap-5 px-2 lg:px-0">
+          <h1 className="font-bold sm:text-3xl text-xl lg:text-5xl bg-gradient-to-r from-[#C939CB] via-[#bfacfcfa] to-[#6a84fdf4] bg-clip-text text-transparent">
             Chat Box Ai
           </h1>
-          <h2 className="font-bold text-xl lg:text-6xl bg-gradient-to-r from-[#7500D1] via-[#bfacfcfa] to-[#C939CB] bg-clip-text text-transparent">
+          <h2 className="font-bold text-xl sm:text-4xl lg:text-6xl bg-gradient-to-r from-[#7500D1] via-[#bfacfcfa] to-[#C939CB] bg-clip-text text-transparent ">
             Generator Image
           </h2>
           <p
-            className={`text-sm lg:w-[750px] text-center lg:text-lg ${GeistMono.className} bg-gradient-to-r from-[#6a84fdf4] to-[#bfacfcfa] bg-clip-text text-transparent`}
+            className={`text-sm lg:w-[750px] text-center md:text-base lg:text-lg ${GeistMono.className} bg-gradient-to-r from-[#6a84fdf4] to-[#bfacfcfa] bg-clip-text text-transparent `}
           >
             Our technology performing fast blockchain (120K TPS) and it has guaranteed AI-based data
             security. Proof of Stake, its consensus algorithm enables unlimited speeds.
           </p>
-          <button />
+          <Button
+            name="get started"
+            styleClass={`bg-gradient-to-r from-[#7500D1] to-[#C939CB] py-2 px-5 lg:py-3 lg:px-8 lg:text-xl rounded-full transition duration-500 pointer cursor-pointer ${GeistMono.className}`}
+          />
         </div>
         <div
-          className="bg-cover w-full h-[120px] lg:h-[600px] bg-no-repeat absolute bottom-0 left-0 -z-0 select-none"
+          className="bg-cover w-full h-[120px] lg:h-[300px] xl:h-[500px] 2xl:h-[600px] bg-no-repeat absolute bottom-0 left-0 -z-0 select-none"
           style={{backgroundImage: `url('./assets/img/robot-background.png')`}}
         />
       </div>
--- frontend/chat-box/src/components/NavbarHome.tsx
+++ frontend/chat-box/src/components/NavbarHome.tsx
@@ -24,12 +24,12 @@ const NavbarHome = () => {
     <>
       <div className="flex justify-between p-5 md:hidden items-center">
         <h3 className={`${audiowide.className} text-2xl text-white`}>Chat Box Ai</h3>
-        <div onClick={() => setIsOpen(!isOpen)}>
+        <div className="cursor-pointer" onClick={() => setIsOpen(!isOpen)}>
           <Menu />
         </div>
       </div>
       <div
-        className={`text-white flex flex-col md:flex-row w-full ${isOpen ? "-translate-x-[0]" : "-translate-x-[500px]"} md:w-full md:translate-x-0 justify-around h-svh md:max-h-[80px] md:justify-between lg:justify-around md:p-4 lg:pt-10 items-center`}
+        className={`text-white flex flex-col md:flex-row w-full ${isOpen ? "-translate-x-[0] bg-gradient-to-r from-[#334155] to-[#bfacfcfa] h-svh absolute z-20" : "-translate-x-[500px]"} md:w-full md:translate-x-0 justify-around  md:max-h-[80px] md:justify-between lg:justify-around md:p-4 lg:pt-10 items-center`}
       >
         <h3 className={`${audiowide.className} hidden md:block text-base md:text-base lg:text-2xl`}>
           Chat Box Ai
